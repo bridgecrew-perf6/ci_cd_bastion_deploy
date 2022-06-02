357396b9b6fa1c3985b673bdd4154d36eb1f4fad
premjagadeesan <premjagadeesan@gmail.com>
2022-06-02 09:07:40 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,3 @@
-aws cloudformation update-stack --stack-name test-stack-1 --template-url https://episyche-ems-assets.s3.ap-south-1.amazonaws.com/ec2_template.yaml --parameters ParameterKey=KeyName,ParameterValue=dataload_appsync
+aws s3 ls
+aws s3 cp bastion.yaml s3://episyche-ems-assets/
+aws cloudformation create-stack --stack-name bastion-stack-1 --template-url https://episyche-ems-assets.s3.ap-south-1.amazonaws.com/bastion.yaml --parameters ParameterKey=KeyName,ParameterValue=dataload_appsync
